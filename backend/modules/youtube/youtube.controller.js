6986fd577964cfcc5065c4265b75d0780f733e63
Subash Karki <subashkarki68@gmail.com>
2024-03-29 03:47:32 +0545

    f

--- backend/modules/youtube/youtube.controller.js
+++ backend/modules/youtube/youtube.controller.js
@@ -1,11 +1,10 @@
-const s3fs = require('@cyclic.sh/s3fs/promises'); // Import the s3fs module
+const s3fs = require('@cyclic.sh/s3fs/promises');
 const ytdl = require('ytdl-core');
 const usetube = require('usetube');
 const { spawn } = require('child_process');
 const ffmpeg = require('ffmpeg-static');
 
-// Initialize s3fs with your S3 bucket name
-const bucketName = process.env.CYCLIC_BUCKET_NAME; // Get bucket name from environment variable
+const bucketName = process.env.CYCLIC_BUCKET_NAME;
 const fs = s3fs(bucketName);
 
 exports.downloadAudio = async (req, res) => {
@@ -19,13 +18,13 @@ exports.downloadAudio = async (req, res) => {
 
         while (totalResults > 0 && !validID) {
             if (validID) {
-                break; // Found a valid video ID, exit the loop
+                break;
             }
 
             console.error('Video ID is not valid, trying next ID...');
             if (result.videos.length > 1) {
-                currentVideoID = result.videos[1].id; // Use the next video ID
-                result.videos.shift(); // Remove the first video from the array
+                currentVideoID = result.videos[1].id;
+                result.videos.shift();
             } else {
                 console.error('No valid video IDs found');
                 return res.status(404).json({
@@ -39,7 +38,6 @@ exports.downloadAudio = async (req, res) => {
         console.log('Video information fetched successfully.');
 
         const audioFormats = ytdl.filterFormats(videoInfo.formats, 'audioonly');
-        // If there are no audio formats available, return error
         if (audioFormats.length === 0) {
             return res.status(404).json({
                 success: false,
@@ -49,30 +47,32 @@ exports.downloadAudio = async (req, res) => {
 
         console.log('Audio formats available for the video.');
 
-        // Choose the highest quality audio format
         const highestAudioFormat = audioFormats[0];
-        // Set filename using the video title
         const fileName = `${videoInfo.videoDetails.title}-${Date.now()}.mp3`;
         console.log('Filename set:', fileName);
 
-        // Set response headers for file download
         console.log('Setting response header');
         res.setHeader(
             'Content-disposition',
             `attachment; filename="${fileName}"`
         );
         res.setHeader('Content-type', 'audio/mpeg');
-        // Pipe the audio stream to ffmpeg to convert it to mp3 and then to response
+
         console.log('Response Header set successfully');
 
-        ytdl(currentVideoID, { format: highestAudioFormat })
-            .pipe(fs.createWriteStream('audio.webm')) // Use s3fs method for creating a write stream
+        const audioStream = ytdl(currentVideoID, {
+            format: highestAudioFormat,
+        });
+
+        const tempFilePath = '/tmp/audio.webm'; // Temporarily store audio on server
+
+        audioStream
+            .pipe(fs.createWriteStream(tempFilePath)) // Store audio on S3
             .on('finish', async () => {
-                // Wait for audio download completion
                 console.log('Audio download completed.');
                 const ffmpegProcess = spawn(ffmpeg, [
                     '-i',
-                    'audio.webm',
+                    tempFilePath,
                     '-codec:a',
                     'libmp3lame',
                     '-q:a',
@@ -80,17 +80,12 @@ exports.downloadAudio = async (req, res) => {
                     fileName,
                 ]);
                 ffmpegProcess.on('close', async () => {
-                    // Wait for audio conversion completion
                     console.log('Audio conversion completed.');
-                    // Stream the converted mp3 file to response
-                    const fileStream = await fs.createReadStream(fileName); // Use s3fs method for creating a read stream
-                    fileStream.pipe(res);
-                    fileStream.on('close', async () => {
-                        // Delete file after sending to the client
-                        await fs.unlink('audio.webm'); // Use s3fs method for deleting files
-                        await fs.unlink(fileName);
-                        console.log('Files deleted successfully.');
-                    });
+                    const fileData = await fs.readFile(fileName);
+                    res.send(fileData);
+                    await fs.unlink(tempFilePath); // Cleanup temp files
+                    await fs.unlink(fileName);
+                    console.log('Files deleted successfully.');
                 });
             })
             .on('error', (err) => {
